08d1467256210ef74de6278e0baf45c3c0531139
Shutto <baw.keepon69@gmail.com>
2021-04-08 19:03:17 +0900

    style memolist screen

--- App.jsx
+++ App.jsx
@@ -1,25 +1,119 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
-import Hello from "./src/components/Hello";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#F0F4F8",
+  },
+  appbar: {
+    width: "100%",
+    height: 104,
+    backgroundColor: "#D34668",
+    justifyContent: "flex-end",
+  },
+  appbarInner: {
     alignItems: "center",
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    fontWeight: "bold",
+    lineHeight: 32,
+    color: "#fff",
+  },
+  appbarRight: {
+    position: "absolute",
+    right: 19,
+    bottom: 16,
+    color: "#fff",
+    opacity: 0.8,
+  },
+  memoListItem: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484",
+  },
+  circleButton: {
+    backgroundColor: "#D34668",
+    width: 64,
+    height: 64,
+    borderRadius: 32,
     justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#fff",
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Hello text="world" />
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>Memo App</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年4月8日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年4月8日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年4月8日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
+      </View>
     </View>
   );
 };
